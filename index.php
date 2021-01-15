12b47f9dc12660f13d249b67c08d9470c67e66db
Marco Guidi <mark.88g@gmail.com>
2021-01-15 16:14:31 +0100

    + inserita funzione stampa consiglio

--- index.php
+++ index.php
@@ -1,11 +1,29 @@
 <?php
+    ini_set('display_errors', 1);
+
     // class movie
     class Movie {
+        // public var
         public $title;
         public $year;
         public $rating;
-    }
 
+        // public function
+        public function stampAdvice() {
+            // get this movie-rating
+            $_rating = $this -> rating;
+
+            if ($_rating < 6) {
+                echo "film non consigliato";
+            } elseif ($_rating < 8) {
+                echo "film consigliato";
+            } else {
+                echo "film consigliatissimo";
+            }
+        } // stamp an advice from rating
+    };
+
+    // movie: il signore degli anelli
     $ilSignoreDegliAnelli = new Movie();
 
     echo "Il signore degli anelli, vuoto";
@@ -13,8 +31,10 @@
 
     $ilSignoreDegliAnelli -> title = "Il signore degli anelli";
     $ilSignoreDegliAnelli -> year = 2001;
-    $ilSignoreDegliAnelli -> rating = 9.2;
+    $ilSignoreDegliAnelli -> rating = 9;
 
     echo "Il signore degli anelli, riempito";
     echo var_dump($ilSignoreDegliAnelli);
+
+    $ilSignoreDegliAnelli -> stampAdvice();
 ?>
